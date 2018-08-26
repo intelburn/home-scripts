fb82a872f6f1d5ddb2aebca645a0a5e02ac039c6
intelburn <nerdacs@gmail.com>
2018-08-26 18:20:51 -0400

    archive.sh now actually works

--- archive.sh
+++ archive.sh
@@ -1,22 +1,26 @@
 #!/bin/bash
 #Fill these in with the paths to the actual synced directories
-ARCHIVE=
-VM=
-ISO=
-PERSONAL=
-SCHOOL=
-WORSHIP=
-CAMERA=
+ARCHIVE=/slow/sync/archive
+VM=/fast/sync/OVA
+ISO=/fast/sync/iso
+PERSONAL=/fast/sync/personal
+SCHOOL=/fast/sync/school
+WORSHIP=/slow/sync/ballston-archive
+CAMERA=/fast/sync/camera
 
 #Copy the camera sync into the personal sync
-rsync --checksum --recursive --exclude=.stfolder $CAMERA $PERSONAL/photos
+rsync --checksum --recursive --exclude=".stfolder" --exclude=".sync" $CAMERA $PERSONAL/photos
 
 #Create Array of dirs for cleaning
 UNCLEAN=($VM $ISO $PERSONAL $SCHOOL $CAMERA)
 #Remove the conflict files and remove them
-for DIR in ${UNCLEAN}; do
-	find $DIR -name *.sync-conflict* -delete
-done
+#for DIR in ${UNCLEAN}; do
+#	find $DIR -name *.sync-conflict* -delete
+#done
 
 #Create Array of dirs for syncing
 SOURCES=($VM $ISO $PERSONAL $SCHOOL $WORSHIP)
+for DIR in ${SOURCES[*]}; do
+	NAME=$(echo $DIR |rev | cut -d"/" -f1 | rev)
+	rsync --checksum --recursive --exclude=".stfolder" --exclude=".sync" --backup --suffix=$(date +%d%m%Y) $DIR $ARCHIVE/$NAME
+done
